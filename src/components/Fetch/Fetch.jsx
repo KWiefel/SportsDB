8639fea731b5edbd80f632c6f5bb1dbc10c6a18d
YacineYldrm <yacine@mailbox.org>
2023-12-19 09:53:56 +0100

    fetch aller liga daten implementiert. fetch und context components im project angelegt. navbar component lBogik implementiert. detail league component filter logik implementiert. context provider im app.jsx eingebaut.

---
+++ src/components/Fetch/Fetch.jsx
@@ -0,0 +1,33 @@
+import { useContext, useEffect, useState } from "react";
+import data from "./../../assets/data/data.json";
+import { AllLeagueContext } from "../Context/Context";
+
+const Fetch = () => {
+    const { setAllLeagueData } = useContext(AllLeagueContext);
+    const [fetchedData, setFetchedData] = useState([]);
+
+    // fetch all league data
+    useEffect(() => {
+        const allPromises = data.map((league) => {
+            const leagueName = league.strLeague;
+            return fetch(`https://thesportsdb.com/api/v1/json/3/search_all_teams.php?l=${leagueName}`)
+                .then(response => response.json())
+                .then(data => data.teams)
+                .catch(error => console.log(error));
+        });
+
+        // wait for all data 
+        Promise.all(allPromises)
+            .then(allData => {
+                setFetchedData(allData);
+            });
+    }, []);
+
+    // update global data state to fetched data
+    useEffect(() => {
+        setAllLeagueData(fetchedData.filter((league) => league));
+    }, [fetchedData, setAllLeagueData]);
+
+};
+
+export default Fetch;
