e1eab39f5ecd2a956d53cff32b5ebb06c7e57036
KWiefel <dev.kwiefel427@gmail.com>
2023-12-20 10:39:41 +0100

    added basic styling to DetailTeam Component

--- src/pages/DetailTeam.jsx
+++ src/pages/DetailTeam.jsx
@@ -1,12 +1,14 @@
 import "./DetailTeam.scss";
 import { Link, useParams } from "react-router-dom";
 import { useContext, useState, useEffect } from "react";
-import { AllLeagueContext, FetchCompleteContext } from "../components/Context/Context";
-
+import {
+  AllLeagueContext,
+  FetchCompleteContext,
+} from "../components/Context/Context";
 
 const DetailTeam = () => {
   // get context
-  const { allLeagueData } = useContext(AllLeagueContext);  
+  const { allLeagueData } = useContext(AllLeagueContext);
   const { fetchStatus } = useContext(FetchCompleteContext);
 
   // component state
@@ -19,90 +21,103 @@ const DetailTeam = () => {
   console.log(useParams());
 
   useEffect(() => {
-      [...allLeagueData].forEach((team) => {
-        if(team.idTeam == "133604")
-        {
-          console.log(team);
-          setFilteredTeam([team])
-        }
-        })
-  }, [fetchStatus])
+    [...allLeagueData].forEach((team) => {
+      if (team.idTeam == "133604") {
+        console.log(team);
+        setFilteredTeam([team]);
+      }
+    });
+  }, [fetchStatus]);
+
+  const backgroundImg = {
+    backgroundImage: `url(${filteredTeam[0]?.strStadiumThumb})`,
+    backgroundColor: "black",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "right",
+    backgroundSize: "50%",
+
+    // width: "300px",
+    // height: "300px",
+  };
 
   return (
     <>
-      <h1>TEAM DETAILS</h1>
-      <h2>{filteredTeam[0]?.strTeam}</h2>
-      <Link to="/"> Back Home</Link>
-      
-      {/* <header className="detailteam__header">
-        <div className="background_img">
-          <h1>{team.strTeam}</h1>
-          <img src={team.strStadiumThumb} alt="Stadium-Picture" />
-          <div className="facts__wrapper">
-            <p className="subtitle">{team.strCountry}</p>
-            <p className="subtitle_description">Country</p>
-            <p className="subtitle">{team.strStadiumLocation}</p>
-            <p className="subtitle_description">Location</p>
-            <p className="subtitle">{team.intFormedYear}</p>
-            <p className="subtitle_description">Established</p>
-            <p className="subtitle">{team.strSport}</p>
-            <p className="subtitle_description">Sport</p>
-          </div>
-        </div>
-      </header>
-      <main className="detailteam__main">
-        <section>
-          <article className="competitions__wrapper">
-            <p className="competitions">Competitions</p>
-            <ul className="competitions_list">
-              <li>{team.strLeague}</li>
-              <li>{team.strLeague2}</li>
-              <li>{team.strLeague3}</li>
-              <li>{team.strLeague4}</li>
-              <li>{team.strLeague5}</li>
-              <li>{team.strLeague6}</li>
-              <li>{team.strLeague7}</li>
-            </ul>
-          </article>
-          <article className="description__wrapper">
-            <span className="description_span">Description</span>
-            <p className="description_text">{team.strDescriptionEN}</p>
-          </article>
-          <div className="logo__wrapper">
-            <img src={team.strTeamBadge} alt="" />
-          </div>
-          <article className="stadium__wrapper">
-            <h3>Stadium</h3>
-            <div className="stadium_flex">
-              <p className="stadium_text">{team.strStadiumDescription}</p>
-              <div className="stadium_facts">
-                <p className="subtitle">{team.strStadium}</p>
-                <p className="subtitle_description">Home</p>
-                <p className="subtitle">{team.intStadiumCapacity}</p>
-                <p className="subtitle_description">Capacity</p>
-              </div>
+      {/* <Link to="/"> Back Home</Link> */}
+      <div className="detailteam__wrapper">
+        <header className="detailteam__header">
+          <div className="background_img" style={backgroundImg}>
+            <h1>{filteredTeam[0]?.strTeam}</h1>
+            <div className="facts__wrapper">
+              <p className="subtitle">{filteredTeam[0]?.strCountry}</p>
+              <p className="subtitle_description">Country</p>
+              <p className="subtitle">{filteredTeam[0]?.strStadiumLocation}</p>
+              <p className="subtitle_description">Location</p>
+              <p className="subtitle">{filteredTeam[0]?.intFormedYear}</p>
+              <p className="subtitle_description">Established</p>
+              <p className="subtitle">{filteredTeam[0]?.strSport}</p>
+              <p className="subtitle_description">Sport</p>
             </div>
-          </article>
-        </section>
-      </main>
-      <footer className="detailteam__footer">
-        <a className="link" href={`https://${team.strWebsite}`}>
-          <p className="subtitle">Website</p>
-        </a>
-        <a className="link" href={`https://${team.strFacebook}`}>
-          <p className="subtitle">Facebook</p>
-        </a>
-        <a className="link" href={`https://${team.strTwitter}`}>
-          <p className="subtitle">Twitter</p>
-        </a>
-        <a className="link" href={`https://${team.strInstagram}`}>
-          <p className="subtitle">Instagram</p>
-        </a>
-        <a className="link" href={`https://${team.strYoutube}`}>
-          <p className="subtitle">Youtube</p>
-        </a>
-      </footer> */}
-
+          </div>
+        </header>
+        <main className="detailteam__main">
+          <section>
+            <article className="competitions__wrapper">
+              <p className="competitions">Competitions</p>
+              <ul className="competitions_list">
+                <li>{filteredTeam[0]?.strLeague}</li>
+                <li>{filteredTeam[0]?.strLeague2}</li>
+                <li>{filteredTeam[0]?.strLeague3}</li>
+                <li>{filteredTeam[0]?.strLeague4}</li>
+                <li>{filteredTeam[0]?.strLeague5}</li>
+                <li>{filteredTeam[0]?.strLeague6}</li>
+                <li>{filteredTeam[0]?.strLeague7}</li>
+              </ul>
+            </article>
+            <article className="description__wrapper">
+              <span className="description_span">Description</span>
+              <p className="description_text">
+                {filteredTeam[0]?.strDescriptionEN}
+              </p>
+            </article>
+            <div className="logo__wrapper">
+              <img src={filteredTeam[0]?.strTeamBadge} alt="" />
+            </div>
+            <article className="stadium__wrapper">
+              <h3>Stadium</h3>
+              <div className="stadium_flex">
+                <p className="stadium_text">
+                  {filteredTeam[0]?.strStadiumDescription}
+                </p>
+                <div className="stadium_facts">
+                  <p className="subtitle">{filteredTeam[0]?.strStadium}</p>
+                  <p className="subtitle_description">Home</p>
+                  <p className="subtitle">
+                    {filteredTeam[0]?.intStadiumCapacity}
+                  </p>
+                  <p className="subtitle_description">Capacity</p>
+                </div>
+              </div>
+            </article>
+          </section>
+        </main>
+        <footer className="detailteam__footer">
+          <a className="link" href={`https://${filteredTeam[0]?.strWebsite}`}>
+            <p className="subtitle">Website</p>
+          </a>
+          <a className="link" href={`https://${filteredTeam[0]?.strFacebook}`}>
+            <p className="subtitle">Facebook</p>
+          </a>
+          <a className="link" href={`https://${filteredTeam[0]?.strTwitter}`}>
+            <p className="subtitle">Twitter</p>
+          </a>
+          <a className="link" href={`https://${filteredTeam[0]?.strInstagram}`}>
+            <p className="subtitle">Instagram</p>
+          </a>
+          <a className="link" href={`https://${filteredTeam[0]?.strYoutube}`}>
+            <p className="subtitle">Youtube</p>
+          </a>
+        </footer>
+      </div>
     </>
   );
 };
